fc3737cc98e043df4e4c938e76a36740e59bc165
Tushar Ubarhande <61948515+explorerTU@users.noreply.github.com>
2022-01-31 16:01:03 +0530

    Add files via upload

---
+++ tv.py
@@ -0,0 +1,3 @@
+with open('readme.txt', 'r') as f:
+    f_contents = f.read()
+    print(f_contents)
---
+++ utr.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() {
+  background(51);
+
+  var len = 100;
+  line(200, height, 200, height - len);
+}
